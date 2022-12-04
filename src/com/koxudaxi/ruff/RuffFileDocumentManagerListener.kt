8bd845b61f50dc77aba8da54aefc93baba80e31d
Koudai Aono <koxudaxi@gmail.com>
2022-12-05 02:13:33 +0900

    Fix undo problem

--- src/com/koxudaxi/ruff/RuffFileDocumentManagerListener.kt
+++ src/com/koxudaxi/ruff/RuffFileDocumentManagerListener.kt
@@ -2,6 +2,9 @@ package com.koxudaxi.ruff
 
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.command.UndoConfirmationPolicy
+import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener
@@ -9,10 +12,10 @@ import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
 
 class RuffFileDocumentManagerListener(private val project: Project) : FileDocumentManagerListener {
+    private val undoManager = UndoManager.getInstance(project)
     override fun beforeDocumentSaving(document: Document) {
-        if (!RuffConfigService.getInstance(project).runRuffOnSave) {
-            return
-        }
+        if (!RuffConfigService.getInstance(project).runRuffOnSave) return
+
         val virtualFile = FileDocumentManager.getInstance().getFile(document) ?: return
         if (!virtualFile.isPyFile) return
         val fileName = virtualFile.name
@@ -20,11 +23,21 @@ class RuffFileDocumentManagerListener(private val project: Project) : FileDocume
         val args = listOf("--exit-zero", "--fix", "--stdin-filename", fileName, "-")
         val stdin = document.text.toByteArray(virtualFile.charset)
         runRuffInBackground(module, stdin, args, "running ruff $fileName") {
-            if (it is String) {
-                runInEdt {
-                    runWriteAction {
-                        document.setText(it)
-                    }
+            if (it !is String) return@runRuffInBackground
+            runInEdt {
+                runWriteAction {
+                    CommandProcessor.getInstance().executeCommand(
+                        project,
+                        {
+                            if (!undoManager.isUndoInProgress) {
+                                document.setText(it)
+                            }
+                        },
+                        "Run ruff",
+                        null,
+                        UndoConfirmationPolicy.DEFAULT,
+                        document
+                    )
                 }
             }
         }
