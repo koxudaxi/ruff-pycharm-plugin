76e34896ca970ce01f3afbd9856f052557ba5fbd
Koudai Aono <koxudaxi@gmail.com>
2023-06-27 01:32:10 +0900

    collect existing noqa code

--- src/com/koxudaxi/ruff/RuffSuppressQuickFix.kt
+++ src/com/koxudaxi/ruff/RuffSuppressQuickFix.kt
@@ -4,18 +4,35 @@ import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiFile
-import com.jetbrains.python.inspections.flake8.Flake8InspectionSuppressor
+import com.intellij.psi.PsiElement
 import com.jetbrains.python.psi.PyUtil
+import com.jetbrains.python.psi.impl.PyPsiUtils
+import java.util.regex.Pattern
 
 class RuffSuppressQuickFix(
     private val code: String,
     private val message: String,
-    private val offset: Int
+    private val textRange: TextRange,
+    private val lineOffset: Int,
 ) :
     LocalQuickFix {
 
+    private val NOQA_COMMENT_PATTERN = Pattern.compile(
+        "# noqa(?::[\\s]?(?<codes>([A-Z]+[0-9]+(?:[,\\s]+)?)+))?.*",
+        Pattern.CASE_INSENSITIVE
+    )
+    data class NoqaCodes(val codes: List<String>?, val noqaStartOffset: Int, val noqaEndOffset: Int)
+    fun extractNoqaCodes(comment: PsiComment): NoqaCodes? {
+        return null
+        val commentText = comment.text ?: return null
+        val noqaOffset = commentText.lowercase().indexOf("# noqa")
+        val noqaStartOffset = comment.textRange.startOffset + noqaOffset
+        val matcher = NOQA_COMMENT_PATTERN.matcher(commentText.substring(noqaOffset)) ?: return NoqaCodes(null, noqaStartOffset, noqaStartOffset + "# noqa".length)
+        val codes = matcher.group("codes")?.split("[,\\s]+".toRegex())?.filter { it.isNotEmpty() }?.distinct() ?: emptyList()
+        return NoqaCodes(codes, noqaStartOffset,noqaStartOffset + matcher.end())
+    }
 
     override fun getFamilyName(): String =
         "Suppress \"$code ${message}\""
@@ -23,32 +40,29 @@ class RuffSuppressQuickFix(
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
         val psiFile = descriptor.psiElement.containingFile
         val comment = PyUtil.`as`(
-            psiFile.findElementAt(offset),
-            PsiComment::class.java
-        )
+            psiFile.findElementAt(textRange.startOffset),
+            PsiElement::class.java
+        )?.let { PyPsiUtils.findSameLineComment(it) }
 
-        val noqaCode = (comment?.let {
-            Flake8InspectionSuppressor.extractNoqaCodes(it)
+        val (existingNoqaCodes, startOffset, endOffset ) = if (comment == null) {
+            Triple(listOf(), null, null)
+        } else {
+            val noqaCodes = extractNoqaCodes(comment)
+            if (noqaCodes?.codes == null) {
+                Triple(listOf(), noqaCodes?.noqaStartOffset, noqaCodes?.noqaEndOffset)
+            } else {
+               Triple(noqaCodes.codes, noqaCodes.noqaStartOffset, noqaCodes.noqaEndOffset)
+            }
+        }
 
-        } ?: emptySet()) + setOf(code)
-//        if (comment != null) {
-//            comment.textRangeInParent
-//        }
         PyUtil.updateDocumentUnblockedAndCommitted(psiFile) { document: Document ->
+            val prefix = if (startOffset != null &&  document.text[startOffset -1] == ' ') ""  else " "
             document.replaceString(
-                offset,
-                offset,
-                " # noqa: ${noqaCode.joinToString(separator = ", ")}"
+                startOffset ?: document.getLineEndOffset(lineOffset),
+                endOffset ?: document.getLineEndOffset(lineOffset),
+                "${prefix}# noqa: ${(existingNoqaCodes + code).joinToString(separator = ", ")}"
             )
         }
-    }
-
-    private fun getNoqaCodes(psiFile: PsiFile, lineLastOffset: Int): Set<String> {
-        val comment = PyUtil.`as`(
-            psiFile.findElementAt(lineLastOffset),
-            PsiComment::class.java
-        )
-        return comment?.let { Flake8InspectionSuppressor.extractNoqaCodes(it) } ?: emptySet()
     }
 
     companion object {
@@ -56,7 +70,8 @@ class RuffSuppressQuickFix(
             return RuffSuppressQuickFix(
                 result.code,
                 result.message,
-                document.getLineEndOffset(result.location.row - 1)
+                document.getStartEndRange(result.location, result.endLocation, 0),
+                result.location.row - 1
             )
         }
     }
