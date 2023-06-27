8b5c7b5cc74e3acfb3bf7d771a29f4430b64ee7e
Koudai Aono <koxudaxi@gmail.com>
2023-06-28 03:17:14 +0900

    Move ruffRun to async

--- src/com/koxudaxi/ruff/RuffNoqaDocumentationTarget.kt
+++ src/com/koxudaxi/ruff/RuffNoqaDocumentationTarget.kt
@@ -5,7 +5,6 @@ import com.intellij.model.Pointer
 import com.intellij.platform.backend.documentation.DocumentationResult
 import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.presentation.TargetPresentation
-import com.intellij.pom.Navigatable
 import com.intellij.psi.PsiComment
 import com.intellij.refactoring.suggested.createSmartPointer
 import com.intellij.refactoring.suggested.startOffset
@@ -13,16 +12,9 @@ import com.intellij.refactoring.suggested.startOffset
 import com.intellij.webSymbols.utils.HtmlMarkdownUtils
 
 class RuffNoqaDocumentationTarget(val psiComment: PsiComment, private val originalElement: PsiComment?, private val offset: Int): DocumentationTarget {
-    override fun computeDocumentationHint(): String? {
-        return computeLocalDocumentation(psiComment, offset)
-    }
-
     override fun computePresentation(): TargetPresentation {
         return targetPresentation(psiComment)
     }
-
-    override val navigatable: Navigatable?
-        get() = psiComment as? Navigatable
 
     private val replaceHtmlTags = listOf(
         "<h1>" to "<h3>",
@@ -37,13 +29,15 @@ class RuffNoqaDocumentationTarget(val psiComment: PsiComment, private val origin
         "</h5>" to "</h6>",
     ).reversed()
     override fun computeDocumentation(): DocumentationResult? {
-        @Suppress("HardCodedStringLiteral") val markdown =
-            computeLocalDocumentation(psiComment,  offset) ?: return null
-        val html = HtmlMarkdownUtils.toHtml(markdown, true) ?: return null
-        val downscaledHtml = replaceHtmlTags.fold(html) { acc, (from, to) ->
-            acc.replace(from, to)
+        val noqaCode = getNoqaCode(psiComment, offset) ?: return null
+        return DocumentationResult.asyncDocumentation {
+            val markdown = runRuff(psiComment.project, listOf("--explain", noqaCode)) ?: return@asyncDocumentation null
+            val html = HtmlMarkdownUtils.toHtml(markdown, true) ?: return@asyncDocumentation null
+            val downscaledHtml = replaceHtmlTags.fold(html) { acc, (from, to) ->
+                acc.replace(from, to)
+            }
+            DocumentationResult.documentation(downscaledHtml)
         }
-        return DocumentationResult.documentation(downscaledHtml)
     }
 
     override fun createPointer(): Pointer<out DocumentationTarget> {
@@ -54,13 +48,12 @@ class RuffNoqaDocumentationTarget(val psiComment: PsiComment, private val origin
             RuffNoqaDocumentationTarget(element, originalElementPtr?.dereference(), offset)
         }
     }
-    private fun computeLocalDocumentation(psiComment: PsiComment, offset: Int): String? {
+    private fun getNoqaCode(psiComment: PsiComment, offset: Int): String? {
         val noqaCodes = extractNoqaCodes(psiComment) ?: return null
         if (noqaCodes.codes?.isNotEmpty() != true) return null
         if (noqaCodes.noqaStartOffset + "# noqa: ".length > offset || noqaCodes.noqaEndOffset < offset) return null
         val targetOffset = offset - psiComment.startOffset
         val startPart = psiComment.text.substring(0, targetOffset).takeLastWhile { it.isLetterOrDigit() }
         val endPart = psiComment.text.substring(targetOffset).takeWhile { it.isLetterOrDigit() }
-        val noqaCode = startPart + endPart
-        return runRuff(psiComment.project, listOf("--explain", noqaCode))
+        return startPart + endPart
     }}
