f6ebddf5f21f33652bb572441fcd93657409fd1e
Koudai Aono <koxudaxi@gmail.com>
2023-12-06 02:16:34 +0900

    Support live config reload for lsp

---
+++ src/com/koxudaxi/ruff/RuffConfigLister.kt
@@ -0,0 +1,53 @@
+package com.koxudaxi.ruff
+
+import RuffLspServerSupportProvider
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
+import com.intellij.openapi.editor.event.EditorFactoryEvent
+import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.util.Key
+import com.intellij.platform.lsp.api.LspServerManager
+import com.intellij.serviceContainer.AlreadyDisposedException
+
+class RuffConfigLister: EditorFactoryListener {
+    private val changeListenerKey = Key.create<DocumentListener>("PyProjectToml.change.listener")
+
+    override fun editorCreated(event: EditorFactoryEvent) {
+        val project = event.editor.project
+        if (project == null || !isRuffConfigEditor(event.editor)) return
+        val listener = object : DocumentListener {
+            override fun documentChanged(event: DocumentEvent) {
+                try {
+                    val document = event.document
+                    if (FileDocumentManager.getInstance().isDocumentUnsaved(document)) {
+                        if (RuffConfigService.getInstance(project).useRuffLsp) {
+                            FileDocumentManager.getInstance().saveAllDocuments()
+                            LspServerManager.getInstance(project)
+                                .stopAndRestartIfNeeded(RuffLspServerSupportProvider::class.java)
+                        }
+                    }
+                } catch (e: AlreadyDisposedException) {
+                }
+            }
+        }
+        with(event.editor.document) {
+            addDocumentListener(listener)
+            putUserData(changeListenerKey, listener)
+        }
+    }
+
+    override fun editorReleased(event: EditorFactoryEvent) {
+        val listener = event.editor.getUserData(changeListenerKey) ?: return
+        event.editor.document.removeDocumentListener(listener)
+    }
+
+    private fun isRuffConfigEditor(editor: Editor): Boolean {
+        val file = editor.document.virtualFile ?: return false
+        if (!file.isRuffConfig) return false
+        val project = editor.project ?: return false
+        return RuffConfigService.getInstance(project).useRuffLsp
+        // TODO: Should detect if the file is in current project
+        }
+}
--- src/com/koxudaxi/ruff/RuffPackageManagerListener.kt
+++ src/com/koxudaxi/ruff/RuffPackageManagerListener.kt
@@ -1,15 +1,19 @@
 package com.koxudaxi.ruff
 
+import RuffLspServerSupportProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.platform.lsp.api.LspServerManager
 import com.jetbrains.python.packaging.PyPackageManager
 
 class RuffPackageManagerListener(project: Project) : PyPackageManager.Listener {
     private val ruffConfigService = RuffConfigService.getInstance(project)
     private val ruffCacheService = RuffCacheService.getInstance(project)
+    private val lspServerManager = LspServerManager.getInstance(project)
     override fun packagesRefreshed(sdk: Sdk) {
         ruffConfigService.projectRuffExecutablePath = findRuffExecutableInSDK(sdk, false)?.absolutePath
         ruffConfigService.projectRuffLspExecutablePath = findRuffExecutableInSDK(sdk, true)?.absolutePath
         ruffCacheService.setVersion()
+        lspServerManager.stopAndRestartIfNeeded(RuffLspServerSupportProvider::class.java)
     }
 }
