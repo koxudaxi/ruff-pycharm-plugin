79e82267e98490122db8ef7c8c00d7b09703383b
Koudai Aono <koxudaxi@gmail.com>
2024-09-02 19:28:55 +0900

    Fix lsp error for wsl

--- src/com/koxudaxi/ruff/lsp/lsp4ij/RuffLanguageServerFactory.kt
+++ src/com/koxudaxi/ruff/lsp/lsp4ij/RuffLanguageServerFactory.kt
@@ -1,7 +1,9 @@
 package com.koxudaxi.ruff.lsp.lsp4ij
+import com.intellij.execution.wsl.WslPath
 import com.intellij.openapi.project.Project
 import com.koxudaxi.ruff.RuffCacheService
 import com.koxudaxi.ruff.RuffConfigService
+import com.koxudaxi.ruff.getRuffExecutable
 import com.koxudaxi.ruff.isInspectionEnabled
 import com.redhat.devtools.lsp4ij.LanguageServerEnablementSupport
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
@@ -26,9 +28,18 @@ class RuffLanguageServerFactory : LanguageServerFactory, LanguageServerEnablemen
         if (!ruffConfigService.useRuffLsp && !ruffConfigService.useRuffServer) return false
         if (!ruffConfigService.useLsp4ij) return false
         if (!isInspectionEnabled(project)) return false
-
         val ruffCacheService = RuffCacheService.getInstance(project)
-        return ruffCacheService.getVersion() != null
+        if (ruffCacheService.getVersion() == null) return false
+        if (ruffConfigService.useRuffServer)  {
+            if (ruffCacheService.hasLsp() != true) return false
+            val ruff = getRuffExecutable(project, ruffConfigService, false) ?: return false
+            if (WslPath.isWslUncPath(ruff.path)) return false
+        }
+        if (ruffConfigService.useRuffLsp) {
+            val ruffLsp = getRuffExecutable(project, ruffConfigService, true) ?: return false
+            if (WslPath.isWslUncPath(ruffLsp.path)) return false
+        }
+        return true
     }
 
     override fun setEnabled(p0: Boolean, project: Project) {
