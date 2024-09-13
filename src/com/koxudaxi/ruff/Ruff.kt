47433aca9b06cf9b3135c188ac1d8470622405f1
Koudai Aono <koxudaxi@gmail.com>
2024-09-14 00:03:09 +0900

    Remove duplicated  getGeneralCommandLine

--- src/com/koxudaxi/ruff/Ruff.kt
+++ src/com/koxudaxi/ruff/Ruff.kt
@@ -273,36 +273,15 @@ fun runCommand(
     commandArgs.executable, commandArgs.project, commandArgs.stdin, *commandArgs.args.toTypedArray()
 )
 
-fun getGeneralCommandLine(executable: File, project: Project?, vararg args: String): GeneralCommandLine? {
-    val projectPath = project?.basePath
-    if (!WslPath.isWslUncPath(executable.path)) {
-     return  getGeneralCommandLine(listOf(executable.path) + args, projectPath)
-    }
-
-    val windowsUncPath = WslPath.parseWindowsUncPath(executable.path) ?: return null
-    val configArgIndex = args.indexOf(CONFIG_ARG)
-    val injectedArgs = if (configArgIndex != -1 && configArgIndex < args.size - 1) {
-        val configPathIndex = configArgIndex + 1
-        val configPath = args[configPathIndex]
-        val windowsUncConfigPath = WslPath.parseWindowsUncPath(configPath)?.linuxPath ?: configPath
-        args.toMutableList().apply { this[configPathIndex] = windowsUncConfigPath }.toTypedArray()
-    } else {
-        args
-    }
-    val commandLine = getGeneralCommandLine(listOf(windowsUncPath.linuxPath) + injectedArgs, projectPath)
-    val options = WSLCommandLineOptions()
-    options.setExecuteCommandInShell(false)
-    options.setLaunchWithWslExe(true)
-    return windowsUncPath.distribution.patchCommandLine<GeneralCommandLine>(commandLine, project, options)
-}
 private fun getGeneralCommandLine(command: List<String>, projectPath: String?): GeneralCommandLine =
     GeneralCommandLine(command).withWorkDirectory(projectPath).withCharset(Charsets.UTF_8)
 
 fun getGeneralCommandLine(executable: File, project: Project?, vararg args: String): GeneralCommandLine? {
     val projectPath = project?.basePath ?: return null
     if (!WslPath.isWslUncPath(executable.path)) {
-        return getGeneralCommandLine(listOf(executable.path) + args, projectPath)
+        return  getGeneralCommandLine(listOf(executable.path) + args, projectPath)
     }
+
     val windowsUncPath = WslPath.parseWindowsUncPath(executable.path) ?: return null
     val configArgIndex = args.indexOf(CONFIG_ARG)
     val injectedArgs = if (configArgIndex != -1 && configArgIndex < args.size - 1) {
