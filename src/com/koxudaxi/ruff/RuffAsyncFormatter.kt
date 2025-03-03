f4c4be8a634dd2d3b47d66fefa2614d3d26226af
Koudai Aono <koxudaxi@gmail.com>
2025-03-04 01:34:34 +0900

    fix: fix format and fix action

--- src/com/koxudaxi/ruff/RuffAsyncFormatter.kt
+++ src/com/koxudaxi/ruff/RuffAsyncFormatter.kt
@@ -6,26 +6,21 @@ import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.psi.PsiFile
-import java.util.*
 
 
-class RuffAsyncFormatterFix : AsyncDocumentFormattingService() {
-    private val FEATURES: MutableSet<FormattingService.Feature> = EnumSet.noneOf(
-        FormattingService.Feature::class.java
-    )
+class RuffAsyncFormatter : AsyncDocumentFormattingService() {
+    private val FEATURES: Set<FormattingService.Feature> = setOf(FormattingService.Feature.FORMAT_FRAGMENTS)
 
-    override fun getFeatures(): MutableSet<FormattingService.Feature> {
+    override fun getFeatures(): Set<FormattingService.Feature> {
         return FEATURES
     }
 
     override fun canFormat(file: PsiFile): Boolean {
         val config = file.project.configService
-        if (config.enableLsp && (config.useRuffLsp || config.useRuffServer)) return false
         return config.runRuffOnReformatCode && file.isApplicableTo
     }
+
     override fun createFormattingTask(request: AsyncFormattingRequest): FormattingTask? {
-        val formattingContext: FormattingContext = request.context
-        val ioFile = request.ioFile ?: return null
         return object : FormattingTask {
             private fun updateText(currentText: String, text: String?) {
                 when {
@@ -34,8 +29,15 @@ class RuffAsyncFormatterFix : AsyncDocumentFormattingService() {
                     else -> request.onTextReady(text)
                 }
             }
+
             override fun run() {
                 runCatching {
+                    val formattingContext: FormattingContext = request.context
+                    val ioFile = request.ioFile
+                    if (ioFile == null) {
+                        request.onTextReady(null)
+                        return@runCatching
+                    }
                     val sourceFile = formattingContext.containingFile.sourceFile
                     val fixCommandArgs =
                         generateCommandArgs(sourceFile, formattingContext.project.FIX_ARGS) ?: return@runCatching
@@ -45,17 +47,30 @@ class RuffAsyncFormatterFix : AsyncDocumentFormattingService() {
                         request.onTextReady(null)
                         return@runCatching
                     }
-                    updateText(currentText, fixCommandStdout)
+                    if (!RuffConfigService.getInstance(formattingContext.project).useRuffFormat) {
+                        updateText(currentText, fixCommandStdout)
+                        return@runCatching
+                    }
+                    val formatCommandArgs = generateCommandArgs(sourceFile, FORMAT_ARGS)
+                    if (formatCommandArgs == null) {
+                        updateText(currentText, fixCommandStdout)
+                        return@runCatching
+                    }
+                    val formatCommandStdout = runRuff(formatCommandArgs, fixCommandStdout.toByteArray())
+                    updateText(currentText, formatCommandStdout)
 
                 }.onFailure { exception ->
                     when (exception) {
-                        is ProcessCanceledException -> { /* ignore */ }
+                        is ProcessCanceledException -> { /* ignore */
+                        }
+
                         else -> {
                             request.onError("Ruff Error", exception.localizedMessage)
                         }
                     }
                 }
             }
+
             override fun cancel(): Boolean {
                 return true
             }
@@ -71,6 +86,6 @@ class RuffAsyncFormatterFix : AsyncDocumentFormattingService() {
     }
 
     override fun getName(): String {
-        return "Ruff Formatter Fix"
+        return "Ruff Formatter"
     }
 }
