d22f797d8c19f998a70d737d30726ec4d9a71cd7
Koudai Aono <koxudaxi@gmail.com>
2025-03-06 04:03:19 +0900

    feat: update plugin version

--- src/com/koxudaxi/ruff/Ruff.kt
+++ src/com/koxudaxi/ruff/Ruff.kt
@@ -31,6 +31,7 @@ import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.jetbrains.python.PythonLanguage
+import com.jetbrains.python.execution.FailureReason
 import com.jetbrains.python.packaging.IndicatedProcessOutputListener
 import com.jetbrains.python.packaging.PyCondaPackageService
 import com.jetbrains.python.packaging.PyExecutionException
@@ -665,6 +666,8 @@ fun checkFixResult(sourceFile: SourceFile, fixResult: String?): String? {
     return fixResult
 }
 
+val PyExecutionException.failureReasonExitCode: Int? get() = (failureReason as? FailureReason.ExecutionFailed)?.output?.exitCode
+
 fun checkFormatResult(sourceFile: SourceFile, formatResult: String?): String? {
     if (formatResult == null) return null
     if (formatResult.isNotBlank()) return formatResult
@@ -672,7 +675,7 @@ fun checkFormatResult(sourceFile: SourceFile, formatResult: String?): String? {
     try {
         runRuff(sourceFile, FORMAT_CHECK_ARGS)
     } catch (e: PyExecutionException) {
-        if (e.exitCode == 1) {
+        if (e.failureReasonExitCode == 1) {
             return formatResult
         }
     }
